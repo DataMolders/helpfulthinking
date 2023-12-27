b5720a61cfdb3a69dab0bd4becd6bd95d518f106
Carwi Barrios <carwelsbarrios@gmail.com>
2023-12-27 16:07:58 +0100

    faqs component acordeon

--- src/components/Faqs/Faqs.tsx
+++ src/components/Faqs/Faqs.tsx
@@ -9,26 +9,36 @@ function Faqs() {
             <div className={styles.container}>
                 <div className={styles.title}>FAQ</div>
                 <div className={styles.accordionContainer}>
-                    <button className="accordion">¿Qué es Helpful Thinking?</button>
-                    <div className="panel">
-                        <p>¿Qué es Helpful Thinking?</p>
-                    </div>
-                    <button className="accordion">¿Cómo funciona Helpful Thinking?</button>
-                    <div className="panel">
-                        <p>Lorem ipsum...</p>
-                    </div>
-                    <button className="accordion">¿Cómo agendar mi primera cita?</button>
-                    <div className="panel">
-                        <p>Lorem ipsum...</p>
-                    </div>
-                    <button className="accordion">¿Cómo acudir a Helpful Thinking?</button>
-                    <div className="panel">
-                        <p>Lorem ipsum...</p>
-                    </div>
-                    <button className="accordion">¿La terapia en línea es efectiva?</button>
-                    <div className="panel">
-                        <p>Lorem ipsum...</p>
-                    </div>
+                    <details className={styles.details}>
+                        <summary>
+                            <div>¿Qué es Helpful Thinking?</div>
+                        </summary>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    </details>
+                    <details className={styles.details}>
+                        <summary>
+                            <div>¿Cómo funciona Helpful Thinking?</div>
+                        </summary>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    </details>
+                    <details className={styles.details}>
+                        <summary>
+                            <div>¿Cómo agendar mi primera cita?</div>
+                        </summary>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    </details>
+                    <details className={styles.details}>
+                        <summary>
+                            <div>¿Cómo acudir a Helpful Thinking?</div>
+                        </summary>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    </details>
+                    <details className={styles.details}>
+                        <summary>
+                            <div>¿La terapia en línea es efectiva?</div>
+                        </summary>
+                        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                    </details>
                 </div>
                 <div className={styles.subtitle}>
                     ¿No encuentras respuesta a tu pregunta? envíanos un email a
